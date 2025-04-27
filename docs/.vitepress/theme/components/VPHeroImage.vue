a1856c4791dd6d112075d0b6cc885dac8efa561e
cuilei0710 <cuilei0710@petalmail.com>
2025-04-27 11:14:34 +0800

    rewrite VPHeroImage

--- docs/.vitepress/theme/components/VPHeroImage.vue
+++ docs/.vitepress/theme/components/VPHeroImage.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
 import { DefaultTheme, VPImage } from 'vitepress/theme';
 import { getRandomInt } from '../../shared/utils';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
-const image = ref<DefaultTheme.ThemeableImage>(`/index/hero_${getRandomInt(1, 11)}.svg?t=${Date.now()}`);
+
+const image = ref<DefaultTheme.ThemeableImage>('');
+
+const updateImage = () => {
+    const randomImageNumber = getRandomInt(1, 11);
+    image.value = `/index/hero_${randomImageNumber}.svg?t=${Date.now()}`;
+};
+
+onMounted(() => {
+    updateImage();
+});
 
 const handleImageError = () => {
     image.value = '/index/hero_1.svg';
