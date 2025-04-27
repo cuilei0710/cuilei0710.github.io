1aaf54a8cbd0c0e6829618f9f246a2b04fa85cfa
cuilei0710 <cuilei0710@petalmail.com>
2025-04-27 19:37:11 +0800

    optimization VPHeroImage

--- docs/.vitepress/theme/components/VPHeroImage.vue
+++ docs/.vitepress/theme/components/VPHeroImage.vue
@@ -1,18 +1,28 @@
 <script setup lang="ts">
 import { DefaultTheme, VPImage } from 'vitepress/theme';
 import { getRandomInt } from '../../shared/utils';
-import { onBeforeMount, onMounted, ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 const image = ref<DefaultTheme.ThemeableImage>('');
 
 const updateImage = () => {
     const randomImageNumber = getRandomInt(1, 11);
-    image.value = `/index/hero_${randomImageNumber}.svg?t=${Date.now()}`;
+    const newImageURL = getImageURL(randomImageNumber);
+    image.value = (image.value !== newImageURL) ?
+        newImageURL :
+        getImageURL(randomImageNumber + 1);
+
+    console.log('newURL' + image.value);
 };
 
-onBeforeMount(() => {
+onMounted(() => {
+    console.log('startURL' + image.value)
     updateImage();
 })
+
+const getImageURL = (index: number) => {
+    return `/index/hero_${index}.svg`;
+}
 
 const handleImageError = () => {
     image.value = '/index/hero_1.svg';
