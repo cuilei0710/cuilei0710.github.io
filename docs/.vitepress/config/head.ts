846985ac62a8acad69164035a4ea3d36f163e548
cuilei0710 <cuilei0710@petalmail.com>
2025-04-27 11:32:38 +0800

    update preload

--- docs/.vitepress/config/head.ts
+++ docs/.vitepress/config/head.ts
@@ -1,7 +1,18 @@
 import { UserConfig } from "vitepress";
 
 const head: UserConfig['head'] = [
-    ['link', { rel: 'icon', href: '/favicon.svg' }]
+    ['link', { rel: 'icon', href: '/favicon.svg' }],
+    ['link', { rel: 'preload', href: '/index/hero_1.svg', as: 'image' }],
+    ['link', { rel: 'preload', href: '/index/hero_2.svg', as: 'image' }],
+    ['link', { rel: 'preload', href: '/index/hero_3.svg', as: 'image' }],
+    ['link', { rel: 'preload', href: '/index/hero_4.svg', as: 'image' }],
+    ['link', { rel: 'preload', href: '/index/hero_5.svg', as: 'image' }],
+    ['link', { rel: 'preload', href: '/index/hero_6.svg', as: 'image' }],
+    ['link', { rel: 'preload', href: '/index/hero_7.svg', as: 'image' }],
+    ['link', { rel: 'preload', href: '/index/hero_8.svg', as: 'image' }],
+    ['link', { rel: 'preload', href: '/index/hero_9.svg', as: 'image' }],
+    ['link', { rel: 'preload', href: '/index/hero_10.svg', as: 'image' }],
+    ['link', { rel: 'preload', href: '/index/hero_11.svg', as: 'image' }],
 ];
 
 export default head;
