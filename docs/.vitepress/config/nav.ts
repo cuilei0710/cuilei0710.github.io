7f204827f3937c802cc5326fa60b51efe41adfa1
cuilei0710 <cuilei0710@petalmail.com>
2025-04-29 13:05:00 +0800

    update nav

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -3,7 +3,6 @@ import { DefaultTheme } from "vitepress";
 const nav: DefaultTheme.Config['nav'] = [
     { text: '首页', link: '/' },
     { text: 'Examples', link: '/markdown-examples' },
-    { text: '荔枝', link: '/lichee' },
 ]
 
 export default nav;
